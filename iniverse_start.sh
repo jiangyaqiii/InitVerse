fc749a38abe5d94cb220733a8b16346f2e44d4db
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-12-24 16:02:47 +0800

    Update iniverse_start.sh

--- iniverse_start.sh
+++ iniverse_start.sh
@@ -4,4 +4,4 @@ cd mining
 
 wget https://github.com/Project-InitVerse/miner/releases/download/v1.0.0/iniminer-linux-x64
 chmod +x iniminer-linux-x64
-./iniminer-linux-x64 --./iniminer-linux-x64 --pool stratum+tcp://$addr.Worker001@pool-core-testnet.inichain.com:32672
+./iniminer-linux-x64 --pool stratum+tcp://$addr.Worker$random_num@pool-core-testnet.inichain.com:32672
