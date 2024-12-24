9ee4f2b66b6fee7a71d51ca4dc11daa278a06b63
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-12-24 15:53:14 +0800

    Create iniverse_start.sh

---
+++ iniverse_start.sh
@@ -0,0 +1,7 @@
+
+mkdir mining
+cd mining
+
+wget https://github.com/Project-InitVerse/miner/releases/download/v1.0.0/iniminer-linux-x64
+chmod +x iniminer-linux-x64
+./iniminer-linux-x64 --./iniminer-linux-x64 --pool stratum+tcp://$addr.Worker001@pool-core-testnet.inichain.com:32672
